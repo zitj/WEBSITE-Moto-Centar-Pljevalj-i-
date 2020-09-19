9482c4019fa63f82997dfeda3d0ea9689f5d8da4
zitj <1eyedea2eyedeas@gmail.com>
2020-09-19 13:51:15 +0200

    starting with responsive on oglas.php

--- oglas.php
+++ oglas.php
@@ -33,8 +33,7 @@ $broj = 1;
     <div class="individualAdvertisement">
         <?php if($oglasBre): ?>
             <h2 id="advertisementTitle"><?php echo htmlspecialchars($oglasBre['title']);?>
-            <h2>godište: <?php echo htmlspecialchars($oglasBre['godiste']);?></h2>
-            <h2>cena: <?php echo htmlspecialchars($oglasBre['price']);?> &euro;</h2>
+          
             
             <div class="swiper-container ">
             <!-- Additional required wrapper -->
@@ -43,7 +42,6 @@ $broj = 1;
             <?php foreach($images as $image): ?>
                 <div class="swiper-slide">
                     <div class="containercheRotirano">
-                        
                         <div class="content">
                             <img src="img/oglasi/<?php echo $oglasBre['title']?>/<?php echo $broj++?>.jpg" alt="">
                         </div><!--end .content -->
@@ -61,27 +59,26 @@ $broj = 1;
             <!-- If we need scrollbar -->
             <!-- <div class="swiper-scrollbar"></div> -->
             </div><!-- end .container-->
-            <h2><?php echo htmlspecialchars($oglasBre['descr']);?></h2>
+            <div class="text">
+            <div class="kolona">
+            <p>Godište: <span><?php echo htmlspecialchars($oglasBre['godiste']);?> god</span></p>
+            <p>Cena: <span><?php echo htmlspecialchars($oglasBre['price']);?> &euro;</span></p>
+            <p>Prešo: <span>xxxxxxx km</span></p>
+            <p>Snaga u kw:<span> xxx kw</span></p>
+            <p>Snaga u ks:<span> xxx ks</span></p>
+            </div>
+            <div class="kolona">
+            <p>Oprema: ima pomocni tockovi</p>
+            <p>Papiri: ima pomocni tockovi</p>
+            <p>Stanje: ima pomocni tockovi</p>
+            <p>Prenos: ima pomocni tockovi</p>
+            <!-- <p><?php echo htmlspecialchars($oglasBre['descr']);?></p> -->
+            </div>
+            </div>
             <button><a href="tel:+381655325532"><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
                 width="24" height="24"
                 viewBox="0 0 172 172"
                 style=" fill:#000000;"><g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><path d="M0,172v-172h172v172z" fill="none"></path><g fill="#ffffff"><path d="M66.40633,113.6275l-18.963,-18.95583c5.7835,-10.97933 12.52017,-19.77283 20.00933,-27.219c7.44617,-7.48917 16.23967,-14.22583 27.219,-20.00933l19.10633,18.99883l36.722,-5.51833v-39.42383c0,0 -2.35067,0 -7.16667,0c-32.5295,0 -63.597,13.18667 -86.24367,35.58967c-22.403,22.64667 -35.58967,53.71417 -35.58967,86.24367c0,4.816 0,7.16667 0,7.16667h39.50267z"></path></g></g></svg>Pozovi</a></button>
-         <!--   
-        <div class="rotatedContainer">
-        <h2><?php echo htmlspecialchars($oglasBre['price']);?></h2>
-            <h2><?php echo htmlspecialchars($oglasBre['godiste']);?></h2>
-        <div class="content">
-            <img src="img/oglasi/<?php echo $oglasBre['title']?>/1.jpg" alt="">
-        </div>
-    </div>
-    <div class="text">
-        <h2><?php echo htmlspecialchars($oglasBre['descr']);?></h2>
-        <button><a href="tel:+381655325532"><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-                width="24" height="24"
-                viewBox="0 0 172 172"
-                style=" fill:#000000;"><g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><path d="M0,172v-172h172v172z" fill="none"></path><g fill="#ffffff"><path d="M66.40633,113.6275l-18.963,-18.95583c5.7835,-10.97933 12.52017,-19.77283 20.00933,-27.219c7.44617,-7.48917 16.23967,-14.22583 27.219,-20.00933l19.10633,18.99883l36.722,-5.51833v-39.42383c0,0 -2.35067,0 -7.16667,0c-32.5295,0 -63.597,13.18667 -86.24367,35.58967c-22.403,22.64667 -35.58967,53.71417 -35.58967,86.24367c0,4.816 0,7.16667 0,7.16667h39.50267z"></path></g></g></svg>Pozovi</a></button>
-    </div>
-        <h2 id="advertisementTitle"><?php echo htmlspecialchars($oglasBre['title']);?></h2> -->
         <?php else: ?>
         <h5>No such advertisement exist!</h5>
         <?php endif; ?>
